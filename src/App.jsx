cbf046931462293f29b7aab6a87d031c81e288af
NT169HU <mano854t@gmail.com>
2020-12-08 15:02:55 +0530

    Added SearchInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import SearchBar from './SearchBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import SearchInput from './components/SearchInput';
 
 function App() {
-
-  const onChange = (event) => {
-    
-  };
-
   return (
     <div className="App">
       <h1>Search Bar Dropdown</h1>
-      <SearchBar onInputChange={onChange}/>
+      <SearchInput />
     </div>
   );
 }
